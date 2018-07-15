1904842d381145b4d9713c05fdddcb340fa26727
Agata <twardowska123@gmail.com>
2018-07-15 15:54:44 +0200

    poprawki

--- src/RightTriangle.java
+++ src/RightTriangle.java
@@ -2,7 +2,7 @@ public class RightTriangle {
 
     public boolean isRightTriangle(Triangle triangle) {
 
-        return ((triangle.getA() * triangle.getA()) + (triangle.getB() * triangle.getB()) == triangle.getC() * triangle.getC());
+             return ((triangle.getA() * triangle.getA()) + (triangle.getB() * triangle.getB()) == triangle.getC() * triangle.getC());
 
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -2,16 +2,25 @@ public class Test {
 
     public static void main(String[] args) {
 
-        Triangle triangle = new Triangle(6, 8, 10);
+        Triangle triangle = new Triangle(10, 8, 6);
 
         boolean isRightTriangle = ((triangle.getA() * triangle.getA()) + (triangle.getB() * triangle.getB()) == triangle.getC() * triangle.getC());
-
+        boolean isRightTriangle1 = ((triangle.getB() * triangle.getB()) + (triangle.getC() * triangle.getC()) == triangle.getA() * triangle.getA());
+        boolean isRightTriangle2 = ((triangle.getA() * triangle.getA()) + (triangle.getC() * triangle.getC()) == triangle.getB() * triangle.getB());
 
         if (isRightTriangle) {
-            System.out.println("Trójkąt jest prosotkątny");
+            System.out.println("Trójkąt jest prostokątny");
+        }
+        else if (isRightTriangle1) {
+            System.out.println("Trójkąt jest prostokątny");
+        }
+        else if (isRightTriangle2) {
+            System.out.println("Trójkąt jest prostokątny");
         } else {
             System.out.println("Trójkąt nie jest prostokątny");
         }
+
     }
 }
+
 
