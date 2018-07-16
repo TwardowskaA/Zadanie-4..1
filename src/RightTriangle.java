318a0edf40cab8164545ea4c3e8fbbe15bf37713
Agata <twardowska123@gmail.com>
2018-07-16 13:16:51 +0200

    poprawki

--- src/RightTriangle.java
+++ src/RightTriangle.java
@@ -2,8 +2,25 @@ public class RightTriangle {
 
     public boolean isRightTriangle(Triangle triangle) {
 
-             return ((triangle.getA() * triangle.getA()) + (triangle.getB() * triangle.getB()) == triangle.getC() * triangle.getC());
+        boolean isRightTriangle = ((triangle.getA() * triangle.getA()) + (triangle.getB() * triangle.getB()) == triangle.getC() * triangle.getC());
+        boolean isRightTriangle1 = ((triangle.getB() * triangle.getB()) + (triangle.getC() * triangle.getC()) == triangle.getA() * triangle.getA());
+        boolean isRightTriangle2 = ((triangle.getA() * triangle.getA()) + (triangle.getC() * triangle.getC()) == triangle.getB() * triangle.getB());
 
+
+        if (isRightTriangle) {
+            System.out.println("Trójkąt jest prostokątny");
+
+        } else if (isRightTriangle1) {
+            System.out.println("Trójkąt jest prostokątny");
+
+        } else if (isRightTriangle2) {
+            System.out.println("Trójkąt jest prostokątny");
+
+        } else {
+            System.out.println("Trókąt nie jest prostokątny");
+
+        }
+
+        return isRightTriangle;
     }
 }
-
